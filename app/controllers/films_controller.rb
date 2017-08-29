7850dc5cafd0ae868e001480b7b5aee94bf08af0
richardk88 <richardk88@hotmail.com>
2017-08-29 16:27:42 -0400

    created full CRUD

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,4 +1,43 @@
 class FilmsController < ApplicationController
   def index
+    @films = Film.all
+  end
+
+  def new
+    @film = Film.new
+  end
+
+  def create
+    @film = Film.create!(film_params)
+
+    redirect_to film_path(@film)
+  end
+
+  def show
+    @film = Film.find(params[:id])
+  end
+
+  def edit
+    @film = Film.find(params[:id])
+  end
+
+  def update
+    @film = Film.find(params[:id])
+    @film.update(film_params)
+
+    redirect_to film_path(@film)
+  end
+
+  def destroy
+    @film = Film.find(params[:id])
+    @film.destroy
+
+    redirect_to films_path
+  end
+
+
+  private
+  def film_params
+    params.require(:film).permit(:title, :genre, :year, :synopsis, :picture)
   end
 end
--- app/models/film.rb
+++ app/models/film.rb
@@ -1,6 +1,6 @@
 class Film < ApplicationRecord
-    # include HTTParty
-    # base_uri ' http://www.omdbapi.com/?apikey=d31f1a94&i=tt2294629'
-
+    include HTTParty
+    base_uri 'http://www.omdbapi.com/?apikey=d31f1a94&i=tt2294629'
+    
     
 end
